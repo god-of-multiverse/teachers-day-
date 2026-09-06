2d69077d19d9d9cd2070fe566f88a31249db2ef6
god-of-war <kalyansuhash@gmail.com>
2026-09-06 10:14:01 +0530

    Invalidate stale page cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "teachers-day-shell-v2";
+const CACHE = "teachers-day-shell-v3";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
@@ -7,7 +7,12 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
 });
 
 self.addEventListener("fetch", (event) => {
